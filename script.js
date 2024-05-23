8a8d175fea0dfaeff243e9a3f9790096abb1092f
li <115918840+Li-su0@users.noreply.github.com>
2024-05-23 14:28:54 +0800

    index.html script.js update

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-    $('.collapsible-header').click(function() {
-        $(this).next('.collapsible-body').slideToggle(10000);
-        $(this).toggleClass('active');
-    });
-});
+    // 当文档准备就绪（即DOM元素加载完成后）执行此函数
 
-// 注意：如果你使用jQuery，确保在HTML文件中也引入了jQuery库
+    $('.collapsible-header').click(function() {
+        // 选择所有具有类名'collapsible-header'的元素，并为它们绑定一个点击事件处理器
+
+        $(this).next('.collapsible-body').slideToggle(500);
+        // 当点击事件发生时，执行以下操作：
+        // 1. $(this) 指的是被点击的'.collapsible-header'元素
+        // 2. .next('.collapsible-body') 选择'.collapsible-header'之后的第一个兄弟元素，且该元素具有类名'collapsible-body'
+        // 3. .slideToggle(500) 对选定的'.collapsible-body'元素执行滑动切换效果（如果它是可见的，则隐藏它；如果它是隐藏的，则显示它），并在500毫秒（0.5秒）内完成这个动画
+
+        $(this).toggleClass('active');
+        // 对被点击的'.collapsible-header'元素切换'active'类。
+        // 如果'.collapsible-header'已经包含'active'类，则移除它；如果不包含，则添加它。
+        // 这通常用于改变'.collapsible-header'的样式，以指示其下的'.collapsible-body'是打开的还是关闭的。
+    });
+    // 结束点击事件处理器的定义
+});
+// 结束文档准备就绪事件处理器的定义
